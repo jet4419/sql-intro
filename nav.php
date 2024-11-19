ca6f554608213850f9562e4d8992b2a8493e9983
jet4419 <curiosojet@gmail.com>
2024-11-19 21:15:01 +0800

    CSS lesson

---
+++ nav.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="nav.css">
+</head>
+<body>
+
+<header>
+	<nav>
+		<h1>LOGO</h1>
+		<ul>
+			<li>
+				<a href="/">HOME</a>	
+			</li>
+			<li>
+				<a href="contact.php">CONTACT</a>
+			</li>
+			<li><a href="about.php">ABOUT</a></li>
+		</ul>
+	</nav>
+</header>
+	
+</body>
+</html>
